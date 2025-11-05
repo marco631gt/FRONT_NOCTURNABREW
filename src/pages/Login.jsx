807232c5e4b903547827722bb3ac0c0c6ca8b3fc
IsselEstrada <st4157@utr.edu.mx>
2025-11-05 14:14:58 -0600

    env

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -50,11 +50,11 @@ const Login = () => {
   }
 
   try {
-    const res = await fetch("https://unjust-tamisha-undeferrably.ngrok-free.dev/api/login", {
+    const res = await fetch(`${import.meta.env.VITE_ENDPOINT}login`, {
       method: "POST",
       headers: {
       "Content-Type": "application/json",
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiJNeUFwcCIsImlhdCI6MTc2MjMwNjcyMn0.SdLp7a8txnblULxR1KhEV22XPSR2aPswJU1WM_5wgFc",
+      "Authorization": `Bearer ${import.meta.env.VITE_APPSECRET}`,
   },
   body: JSON.stringify({ email, password }),
 });
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -49,11 +49,11 @@ const Register = () => {
   }
 
   try {
-    const res = await fetch("https://unjust-tamisha-undeferrably.ngrok-free.dev/api/users/create", {
+    const res = await fetch(`${import.meta.env.VITE_ENDPOINT}users/create`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHAiOiJNeUFwcCIsImlhdCI6MTc2MjMwNjcyMn0.SdLp7a8txnblULxR1KhEV22XPSR2aPswJU1WM_5wgFc", 
+        "Authorization": `Bearer ${import.meta.env.VITE_APPSECRET}`, 
       },
       body: JSON.stringify({ name, email, password }),
     });
